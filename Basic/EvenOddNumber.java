df2afad136e6ceee2ba35be07bf1afb8b13cc741
anjansharma06 <anjanasharma6912@gmail.com>
2024-02-14 19:21:10 +0530

    Even Odd Number Program

---
+++ Basic/EvenOddNumber.java
@@ -0,0 +1,9 @@
+package Basic;
+
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        int num =5;
+        if(num % 2 == 0) System.out.println("Even Number");
+        else System.out.println("Odd Number");
+    }
+}
